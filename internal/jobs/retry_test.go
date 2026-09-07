01992a639b1f3cf7ac74e5b0d2f2b29fc59ec454
Jason <jsnmar@gmail.com>
2026-09-07 11:32:50 -0400

    Add simple retry test and CI workflow

---
+++ internal/jobs/retry_test.go
@@ -0,0 +1,15 @@
+package jobs
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRetryDelay(t *testing.T) {
+	got := RetryDelay(1)
+	want := 1 * time.Second
+
+	if got != want {
+		t.Errorf("RetryDelay(1) = %v, want %v", got, want)
+	}
+}
